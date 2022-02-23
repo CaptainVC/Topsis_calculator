86d81cf90ddd191489cf9ee13ac2c81303c976a3
CaptainVC <100281435+CaptainVC@users.noreply.github.com>
2022-02-23 22:41:00 +0530

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -1,5 +1,5 @@
 ## set email and password of the account from which you want to send email
 ## and also enable less secure app access in google account settings
 
-email = "vchopra_be19@thapar.edu"
-password = "Wolverina2099*"
+email = "email"
+password = "password"
